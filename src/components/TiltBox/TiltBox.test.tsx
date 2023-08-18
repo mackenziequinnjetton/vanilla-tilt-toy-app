9a80da92d3e99a1e9983fbbb24e5b5e604f50a0b
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-18 07:21:18 -0600

    Add TiltBox test, WIP making it green

--- src/components/TiltBox/TiltBox.test.tsx
+++ src/components/TiltBox/TiltBox.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import TiltBox from './TiltBox';
 
@@ -7,4 +7,11 @@ test('renders TiltBox', () => {
   render(<TiltBox />);
   const tiltBoxElement = screen.getByText(/TiltBox/i);
   expect(tiltBoxElement).toBeInTheDocument();
+});
+
+test("user can mouse over the TiltBox and see the box tilt", () => {
+  render(<TiltBox />);
+  const tiltBoxElement = screen.getByText(/TiltBox/i);
+  fireEvent.mouseOver(tiltBoxElement);
+  expect(tiltBoxElement).toHaveStyle("transform: rotateX(10deg) rotateY(10deg)");
 });
--- src/components/TiltBox/TiltBox.tsx
+++ src/components/TiltBox/TiltBox.tsx
@@ -1,9 +1,13 @@
-// import "./TiltBox.css";
+import "./TiltBox.css";
+
+import { useRef } from "react";
 
 const TiltBox = () => {
+  const tiltBoxRef = useRef(null);
+
   return (
     <>
-      <div id="tilt-box">
+      <div id="tilt-box" ref={tiltBoxRef}>
         TiltBox
       </div>
     </>
