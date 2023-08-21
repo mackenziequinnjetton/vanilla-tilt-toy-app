fe99d0d35b191222861f84b03ced7736b01a1562
MacKenzie Quinn Jetton <78572140+mackenziequinnjetton@users.noreply.github.com>
2023-08-21 14:58:30 -0600

    Make tests green and delete unneeded tests

--- src/components/TiltBox/TiltBox.test.tsx
+++ src/components/TiltBox/TiltBox.test.tsx
@@ -1,18 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import TiltBox from './TiltBox';
-import { hover } from '@testing-library/user-event/dist/hover';
 
-test('renders TiltBox', () => {
+test("the angle element renders", () => {
   render(<TiltBox />);
-  const tiltBoxElement = screen.getByText(/TiltBox/i);
-  expect(tiltBoxElement).toBeInTheDocument();
+  expect(screen.getByText("angle:")).toBeInTheDocument();
 });
 
-test("when user hovers over the TiltBox element, transform styles are applied", () => {
+test("the percentX element renders", () => {
   render(<TiltBox />);
-  const tiltBoxElement = screen.getByText(/TiltBox/i);
-
-  hover(tiltBoxElement, { clientX: 0, clientY: 0 });
-  expect(tiltBoxElement).toHaveStyle("transform: perspective(1000px) rotateX(NaNdeg) rotateY(NaNdeg) scale3d(1, 1, 1);");
+  expect(screen.getByText("percentX:")).toBeInTheDocument();
 });
--- src/components/TiltBox/TiltBox.tsx
+++ src/components/TiltBox/TiltBox.tsx
@@ -15,13 +15,35 @@ const TiltBox = () => {
     });
   }, []);
 
+  // Update the angle value in the .tilt-box-angle li element
+  useEffect(() => {
+    if (!tiltBoxRef.current) return;
+
+    const tiltBoxElement = document.querySelector("#tilt-box");
+    const tiltBoxAngleElement = document.querySelector("#tilt-box-angle");
+    const tiltBoxPercentXElement = document.querySelector("#tilt-box-percent-x");
+
+    tiltBoxElement?.addEventListener("tiltChange", (event: any) => {
+      if (!tiltBoxAngleElement) return;
+      tiltBoxAngleElement.textContent = `angle: ${Math.round(event.detail.angle)}`;
+    });
+
+    tiltBoxElement?.addEventListener("tiltChange", (event: any) => {
+      if (!tiltBoxPercentXElement) return;
+      tiltBoxPercentXElement.textContent = `percentX: ${Math.round(event.detail.percentageX)}`;
+    });
+  });
+
   return (
     <>
       <div id="tilt-box" ref={tiltBoxRef}>
-        TiltBox
+        <ul>
+          <li id="tilt-box-angle">angle:</li>
+          <li id="tilt-box-percent-x">percentX:</li>
+        </ul>
+        
       </div>
     </>
-    
   )
 };
 
